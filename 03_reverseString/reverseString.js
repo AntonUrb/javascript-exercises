273509d51192a985b8c2f82ee8fd39969d74b658
AntonUrb <anton.urban.est@gmail.com>
2024-08-11 14:16:55 +0300

    Complete third

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseArray = ""
+    for (let i = 0; i < string.length; i++) {
+        reverseArray += string[(string.length - i - 1)]
+    }
+    return reverseArray
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// 
