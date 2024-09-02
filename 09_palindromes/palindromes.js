2c9b5dad5cf59ca77205a5d7c7544be8fd79a65c
AntonUrb <anton.urban.est@gmail.com>
2024-09-02 13:57:20 +0300

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let alpha = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    let array = str
+        .toLowerCase()
+        .split("")
+        .filter(character => alpha.includes(character))
+
+    return array.join("") === array.reverse().join("")
 };
 
 // Do not edit below this line
