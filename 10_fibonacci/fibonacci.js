bea414b515acaf512878b3a0345028f3a34db304
AntonUrb <anton.urban.est@gmail.com>
2024-09-02 15:22:11 +0300

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    const int = parseInt(num)
 
+    if (int === 0) {
+        return 0
+    } else if (int < 0) {
+        return "OOPS"
+    }
+
+    let fiboArray = [0, 1]
+
+    for (let i = 2; i <= int; i++) {
+        fiboArray[i] = fiboArray[i - 1] + fiboArray[i - 2]
+        fiboArray.push()
+    }
+    return fiboArray[fiboArray.length - 1]
 };
 
 // Do not edit below this line
