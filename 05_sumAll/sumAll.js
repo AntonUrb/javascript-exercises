dc42199ea60e5d6b00af56eb952496aaed217b1c
AntonUrb <anton.urban.est@gmail.com>
2024-09-02 12:14:38 +0300

    Complete calculator

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function (num1, num2) {
     if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
         return 'ERROR'
     }
-    
+
     if (num1 > num2) {
         let temp = num1
         num1 = num2
@@ -10,11 +10,11 @@ const sumAll = function (num1, num2) {
 
     }
 
-        let sum = 0
-        for (num1; num1 <= num2; num1++) {
-            sum += num1
-        }
-        return sum
+    let sum = 0
+    for (num1; num1 <= num2; num1++) {
+        sum += num1
+    }
+    return sum
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, num) => sum += num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, num) => sum *= num)
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  let result = 1
+
+  for (let i = 1; i <= power; i++) {
+    result *= num
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1
+
+  for (let i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
